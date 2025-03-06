b6d7a08bf55bc3f99c7df75a1396515a545252ca
shray9 <shray.modi45@gmail.com>
2025-03-06 09:46:19 +0530

    changes

--- .github/workflows/test_app.py
+++ .github/workflows/test_app.py
@@ -1,3 +1,11 @@
-# tests/test_app.py
-def test_sample():
-    assert 1 + 1 == 2
+import unittest
+
+def add(a, b):
+    return a + b
+
+class TestSample(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(add(2, 3), 5)
+
+if __name__ == '__main__':
+    unittest.main()
