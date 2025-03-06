18da4043dc7f032298100f9b86f4c8288c9cd05a
shray9 <shray.modi45@gmail.com>
2025-03-06 09:48:54 +0530

    python code chnaged

--- .github/workflows/test_app.py
+++ .github/workflows/test_app.py
@@ -1,11 +1,4 @@
-import unittest
+# tests/test_app.py
+def test_sample():
+    assert 1 + 1 == 2
 
-def add(a, b):
-    return a + b
-
-class TestSample(unittest.TestCase):
-    def test_addition(self):
-        self.assertEqual(add(2, 3), 5)
-
-if __name__ == '__main__':
-    unittest.main()
